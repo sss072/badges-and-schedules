e23c17893cb6cfb6004353383ac670d5fd23129d
Sami <samsabeeh6@gmail.com>
2021-01-24 01:26:24 -0500

    commit tree c15e98747bab97f4cf032225af01efec87edc64f

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def badge_maker(name_given)
+    "Hello, my name is #{name_given}."
+end 
+
+def batch_badge_creator(array_names)
+    new_array = []
+    array_names.each do |element|
+        new_array << badge_maker(element)
+    end 
+    new_array
+end 
+
+def assign_rooms(array_speakers)
+    new_array = []
+    array_speakers.each.with_index(1) do |element, index|
+        new_array << "Hello, #{element}! You'll be assigned to room #{index}!"
+    end 
+    new_array
+end 
+
+def printer(array)
+    batch_badge_creator(array).each do |element|
+        puts element 
+    end 
+    assign_rooms(array).each do |element|
+        puts element
+    end 
+
+end 
